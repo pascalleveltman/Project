26d2ce1df940db9a3919c6a90e39f10458998b28
Pascalle Veltman <pascalleveltman@hotmail.nl>
2019-06-20 18:07:54 +0200

    site

--- bubble.js
+++ bubble.js
@@ -8,10 +8,17 @@ window.onload = function () {
   // call plot function with given data file
   Promise.all(requests).then(function(response) {
 
-      legend()
+      legend();
       slider(response[0]);
       bubble(response[0], "2005", 0);
+      line_chart(response[0], "Belgium", 0);
+      scatter_plot(response[0], 0);
+      // scatter_plot(response[0], "2005", $('#x-value').val(), $('#y-value').val());
 
+      d3.select("button")
+        .on("click", function(d){
+          scatter_plot(response[0], 1)
+        })
   });
 
 };
@@ -24,8 +31,8 @@ function slider(data){
   });
 
   // set the size of the slider
-  var width = 600;
-  var height = 100;
+  var width = 700;
+  var height = 80;
 
   // set slider
   var time_slider = d3.sliderBottom()
@@ -37,11 +44,10 @@ function slider(data){
        .tickValues(years)
        .default(new Date(2000, 10, 3))
        .on('onchange', val => {
-            var gone = 1
            d3.select('p#value-time').text(d3.timeFormat('%Y')(val));
 
-           console.log(val.getFullYear())
-           bubble(data, val.getFullYear(), gone)
+           bubble(data, val.getFullYear(), 1);
+           scatter_plot(data, 1);
 
        });
 
@@ -51,7 +57,7 @@ function slider(data){
       .attr("height", height)
       .attr("width", width)
     .append("g")
-      .attr('transform', 'translate(20,20)');
+      .attr('transform', 'translate(80,20)');
 
   // call slider
   g.call(time_slider);
@@ -70,7 +76,7 @@ function bubble(data, year, gone){
   year_data = data[0][String(year)]
 
   // set the diameter of the chart
-  var diameter = 600;
+  var diameter = 700;
 
   // add chart in the right size
   var svg = d3.select("#chart")
@@ -79,6 +85,7 @@ function bubble(data, year, gone){
         .attr("width", diameter)
         .attr("id", "bubble_chart")
       .append("g")
+        .attr("transform", "translate(50,0)")
 
   // position the bubbles
   var simulation = d3.forceSimulation()
@@ -130,6 +137,8 @@ function bubble(data, year, gone){
     return color_bubble
   }
 
+  var group_data = [year_data, data];
+
   // create the bubbles
   var circles = svg.selectAll(".bubble")
     .data(year_data)
@@ -141,7 +150,10 @@ function bubble(data, year, gone){
       .attr("fill", function(d){
         return set_color(d.EFP, d.REG, 0)
       })
-      .on("mouseover.increase", function(d, i) {
+      .on("mouseover", function(d, i) {
+
+        tooltip.style("display", null);
+
         d3.select(this)
           .attr("stroke", function(d){
             return set_color(d.EFP, d.REG, 1)
@@ -149,16 +161,25 @@ function bubble(data, year, gone){
 			     .attr("stroke-width", 2 )
            .attr("r", function(d){
              return radius_scale(d.APR)
-           })})
-      .on("mouseover.tool", show_tooltip)
-      .on("mousemove", move_tooltip)
-      .on("mouseout.decrease", function(d, i) {
+      })})
+      .on("mousemove", function(d){
+        var x_pos = d3.mouse(this)[0] - 15
+        var y_pos = d3.mouse(this)[1] - 25
+        tooltip.attr("transform", "translate(" + x_pos + "," + y_pos + ")")
+        tooltip.select("text").text(d.Country)
+      })
+      .on("mouseout", function(d, i) {
+
+        tooltip.style("display", "none");
+
         d3.select(this).attr("stroke", "none")
            .attr("r", function(d){
              return radius_scale(d.APR)
            })
          })
-      .on("mouseout.tool", hide_tooltip);
+       .on("click", function(d) {
+         line_chart(data, d.Country, 1);
+       });
 
   simulation.nodes(year_data)
     .on('tick', ticked)
@@ -174,42 +195,17 @@ function bubble(data, year, gone){
   }
 
   // add a tooltip
-  var tooltip = d3.select("chart")
-    .append("div")
-      .style("opacity", 0)
-      .style("background-color", "black")
-      .style("border-radius", "5px")
-      .style("padding", "10px")
-      .style("color", "white")
+  var tooltip = svg.append("g")
+      .attr("class", "tooltip")
 
-  // move the tooltip
-  var show_tooltip = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-    tooltip
-      .style("opacity", 1)
-      .html("Country: " + d.Country)
-      .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px")
-  }
-  var move_tooltip = function(d) {
-    tooltip
-      .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px")
-  }
-  var hide_tooltip = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-      .style("opacity", 0)
-  }
+  tooltip.append("text")
+    // .attr("class", "tooltiptext")
 }
 
 function legend(){
 
   // set sizes of legend
-  var height = 150;
+  var height = 100;
   var width = 600;
 
   var regions = ["North Europe", "East Europe", "South Europe", "West Europe"];
@@ -222,23 +218,23 @@ function legend(){
         .attr("width", width)
         .attr("id", "bubble_chart")
     .append("g")
-        .attr('transform', 'translate(20,20)')
+        .attr('transform', 'translate(0,0)')
 
-  for (i in [0, 1, 2, 3, 4, 5, 6]){
+  for (i in [8, 7, 6, 5, 4, 3, 2, 1, 0]){
     svg.selectAll("my_bars")
       .data(colors)
         .enter()
       .append("rect")
         .attr("x", function(d){
-          return i * 40
+          return (i-8) * - 20
         })
         .attr("y", function(d){
-          return colors.indexOf(d) * 30;
+          return colors.indexOf(d) * 20;
         })
         .attr("width", function(d){
-          return 40;
+          return 20;
         })
-        .attr("height", 20)
+        .attr("height", 10)
         .attr("fill", function(d){
           return d[i];
         })
@@ -248,20 +244,359 @@ function legend(){
     .data(regions)
       .enter()
     .append("text")
-      .attr("x", 280)
+      .attr("x", 180)
       .attr("y", function(d){
-        return 16 + regions.indexOf(d) * 30;
+        return 10 + regions.indexOf(d) * 20;
       })
       .attr("width", 20)
       .attr("height", 20)
-      .attr("fill", function(d){
-        return colors[regions.indexOf(d)][4];
-      })
+      // .attr("fill", function(d){
+      //   return colors[regions.indexOf(d)][4];
+      // })
       .attr("font-family", "Arial")
+      .attr("font-size", 12)
       .text(function(d){
         return d
       });
 
+}
 
+function line_chart(data_all, country, gone){
+
+  // check if there is a chart that has to be removed
+  if (gone == 1){
+    d3.select("#line_chart").remove();
+  }
+
+  // select data  and make empty lists
+  var data = data_all[0];
+  var years_list = [];
+  var APR_list = [];
+  var EFP_list = [];
+
+  // fill lists with right country data to use
+  for (var i = 2005; i <= 2016; i++) {
+       years_list.push(i);
+  }
+  for (var i = 2005; i <= 2016; i++) {
+    for (var j = 0; j < data[String(i)].length; j++) {
+      if (data[String(i)][j]["Country"] == country) {
+        APR_list.push(data[String(i)][j]["APR"]);
+        EFP_list.push(data[String(i)][j]["EFP"])
+      }
+    }
+  }
+
+  // set sizes and margins of line chart
+  var total_width = 650;
+  var total_height = 500;
+  var margin = {top: 80, right: 100, bottom: 80, left: 100}
+  var width = total_width - margin.left - margin.right;
+  var height = total_height - margin.top - margin.bottom;
+
+  // add the svg element
+  var svg = d3.select("#linechart")
+    .append("svg")
+        .attr("height", total_height)
+        .attr("width", total_width)
+        .attr("id", "line_chart")
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // set the APR scale for the axis
+  var x_sc_APR = d3.scaleLinear()
+      .domain([2005, 2016])
+      .range([0, width])
+      .nice();
+  var y_sc_APR = d3.scaleLinear()
+       .domain([Math.min(...APR_list) - 0.5, Math.max(...APR_list)])
+       .range([height, 0])
+       .nice();
+
+  // set the EFP scale for the axis
+  var y_sc_EFP = d3.scaleLinear()
+       .domain([Math.min(...EFP_list) - 3, Math.max(...EFP_list)])
+       .range([height, 0])
+       .nice();
+
+  // set the line generator for APR
+  var APR_red = d3.schemeReds[9]
+  var line_APR = d3.line()
+      .x(function(d, i) {
+        return x_sc_APR(2005 + i);
+      })
+      .y(function(d) {
+        return y_sc_APR(parseFloat(d));
+      })
+      .curve(d3.curveMonotoneX)
+
+  // set the line generator for EFP
+  var EFP_green = d3.schemeGreens[9]
+  var line_EFP = d3.line()
+      .x(function(d, i) {
+        return x_sc_APR(2005 + i);
+      })
+      .y(function(d) {
+        return y_sc_EFP(parseFloat(d));
+      })
+      .curve(d3.curveMonotoneX)
+
+  // draw the APR line and dots
+  svg.append("path")
+    .datum(APR_list)
+    .attr("d", line_APR)
+    .style("fill", "none")
+    .style("stroke", APR_red[3])
+    .style("stroke-width", 2);
+
+  svg.selectAll(".dot")
+    .data(APR_list)
+    .enter()
+    .append("circle") // Uses the enter().append() method
+      .attr("cx", function(d, i) {
+        return x_sc_APR(2005 + i)
+      })
+      .attr("cy", function(d) {
+        return y_sc_APR(parseFloat(d))
+      })
+      .attr("r", 3)
+      .attr("fill", APR_red[3])
+
+  // draw the EFP line and dots
+  svg.append("path")
+    .datum(EFP_list)
+    .attr("d", line_EFP)
+    .style("fill", "none")
+    .style("stroke", EFP_green[3])
+    .style("stroke-width", 3);
+
+  svg.selectAll(".dot")
+    .data(EFP_list)
+    .enter()
+    .append("circle") // Uses the enter().append() method
+      .attr("cx", function(d, i) {
+        return x_sc_APR(2005 + i)
+      })
+      .attr("cy", function(d) {
+        return y_sc_EFP(parseFloat(d))
+      })
+      .attr("r", 3)
+      .attr("fill", EFP_green[3])
+
+  // text for the x axis
+  svg.append("text")
+    .attr("transform", "translate(" + (total_width/2 - margin.left) + "," + (height + margin.bottom/2) + ")")
+    .attr("font-family", "Arial")
+    .style("text-anchor", "middle")
+    .attr("font-size", 14)
+    .text("Years");
+
+  // text for the y axis
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", - height/2 )
+    .attr("y", - 40)
+    .attr("font-family", "Arial")
+    .style("text-anchor", "middle")
+    .attr("font-size", 14)
+    .text("Average flights per habitant");
+  // text for the y axis
+  svg.append("text")
+    .attr("transform", "rotate(90)")
+    .attr("x", height/2 )
+    .attr("y", - width - margin.left/2)
+    .attr("font-family", "Arial")
+    .style("text-anchor", "middle")
+    .attr("font-size", 14)
+    .text("Ecological footprint");
+
+  // draw x axis
+  var x_axis = d3.axisBottom(x_sc_APR)
+    .ticks(10);
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(x_axis);
+  // draw left APR axis
+  var y_axis_l = d3.axisLeft(y_sc_APR)
+  svg.append("g")
+      .call(y_axis_l)
+      .style("fill", APR_red[3]);
+  // draw right EFP axis
+  var y_axis_r = d3.axisRight(y_sc_EFP)
+  svg.append("g")
+      .attr("transform", "translate(" + width + ",0)")
+      .call(y_axis_r)
+      .style("fill", EFP_green[3]);
+
+}
+
+function scatter_plot(data, gone){
+
+  // select plot data
+  var x_var = "EFP";
+  var y_var = "APR";
+  var year = "2005";
+
+  // check if there is a chart that has to be removed
+  if (gone == 1){
+    d3.select("#scatter_plot").remove();
+    x_var = $('#x-value').val();
+    y_var = $('#y-value').val();
+    year = $('.slider .parameter-value text').html();
+    console.log(x_var, y_var, year)
+  }
+
+  // select the right data by its year
+  var year_data = data[0][String(year)];
+
+  // set sizes and margins of line chart
+  var total_width = 650;
+  var total_height = 500;
+  var margin = {top: 80, right: 100, bottom: 80, left: 100}
+  var width = total_width - margin.left - margin.right;
+  var height = total_height - margin.top - margin.bottom;
+
+  // add chart in the right size
+  var svg = d3.select("#scatterplot")
+    .append("svg")
+        .attr("height", total_height)
+        .attr("width", total_width)
+        .attr("id", "scatter_plot")
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // set the scale for the x axis and y axis
+  var x_scale = d3.scaleLinear()
+      .domain([d3.min(year_data, function(d) {
+          return d[x_var];
+       }),
+       d3.max(year_data, function(d) {
+          return d[x_var];
+       })])
+      .range([0, width])
+      .nice();
+
+  var y_scale = d3.scaleLinear()
+      .domain([d3.min(year_data, function(d) {
+          return d[y_var];
+       }),
+       d3.max(year_data, function(d) {
+          return d[y_var];
+       })])
+      .range([height, 0])
+      .nice();
+
+  // set color of north, east, south or west european country
+  function set_color(region, mouse){
+    color_index = 4
+    if (mouse == 1){
+      color_index = color_index + 2;
+    }
+
+    if (region == "N"){
+      return d3.schemeBlues[9][color_index];
+    }
+    else if (region == "E"){
+      return d3.schemePurples[9][color_index];
+    }
+    else if (region == "S"){
+      return d3.schemeReds[9][color_index];
+    }
+    else if (region == "W"){
+      return d3.schemeGreens[9][color_index];
+    }
+  }
+
+  // add dots
+  svg.selectAll(".dot")
+   .data(year_data)
+   .enter()
+   .append("circle")
+     .attr("cx", function(d) {
+       return x_scale(d[x_var])
+     })
+     .attr("cy", function(d) {
+       return y_scale(d[y_var])
+     })
+     .attr("r", 5)
+     .attr("fill", function(d){
+       return set_color(d["REG"], 0)
+     })
+     .on("mouseover", function(d, i) {
+
+       tooltip.style("display", null);
+
+       d3.select(this)
+         .attr("stroke", function(d){
+           return set_color(d["REG"], 1)
+         })
+          .attr("stroke-width", 2 )
+          .attr("r", 6)})
+     .on("mousemove", function(d){
+       var x_pos = d3.mouse(this)[0] - 15
+       var y_pos = d3.mouse(this)[1] - 15
+       tooltip.attr("transform", "translate(" + x_pos + "," + y_pos + ")")
+       tooltip.select("text").text(d.Country)
+     })
+     .on("mouseout", function(d, i) {
+
+       tooltip.style("display", "none");
+
+       d3.select(this).attr("stroke", "none")
+          .attr("r", 5)
+        })
+      .on("click", function(d) {
+        line_chart(data, d.Country, 1);
+      });
+
+   // draw x axis
+   var x_axis = d3.axisBottom(x_scale)
+    .ticks(10);
+   svg.append("g")
+       .attr("transform", "translate(0," + height + ")")
+       .call(x_axis);
+   // draw y axis
+   var y_axis = d3.axisLeft(y_scale)
+    .ticks(5);
+   svg.append("g")
+       .call(y_axis)
+
+   function title(afk){
+     if (afk == "APR"){
+       return "Average Flights per Habitant";
+     }
+     else if (afk == "EFP"){
+       return "Ecological Footprint";
+     }
+     else if (afk == "RPR"){
+       return "Average Train Rides per Habitant";
+     }
+     else if (afk == "EEX"){
+       return "Government Environmental Expenditure";
+     }
+   }
+
+   // text for the x axis
+   svg.append("text")
+     .attr("transform", "translate(" + (width/2) + "," + (height + margin.bottom/2) + ")")
+     .attr("font-family", "Arial")
+     .style("text-anchor", "middle")
+     .attr("font-size", 14)
+     .text(title(x_var));
+   // text for the y axis
+   svg.append("text")
+     .attr("transform", "rotate(-90)")
+     .attr("x", - height/2)
+     .attr("y", - 40)
+     .attr("font-family", "Arial")
+     .attr("font-size", 14)
+     .style("text-anchor", "middle")
+     .text(title(y_var));
+
+   // add a tooltip
+   var tooltip = svg.append("g")
+
+   tooltip.append("text")
+      .style("class", "tooltip")
 
 }
